899b7305933fe6d76833d6d414913c5e44678551
Denis Enes <denisenes68@gmail.com>
2021-09-22 13:24:49 +0700

    Task1

---
+++ Task1/src/main/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+     static class PrinterThread extends Thread {
+        @Override
+        public void run() {
+            printLoop("Child");
+        }
+    }
+
+    public static void printLoop(String str) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("I'm " + str);
+        }
+    }
+
+    public static void main(String [] args) {
+        PrinterThread printer = new PrinterThread();
+        printer.start();
+        printLoop("Parent");
+    }
+}
